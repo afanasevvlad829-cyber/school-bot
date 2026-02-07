d3987dff3137bee7ebf1c4baaa88b7eeab7af307
afanasevvlad829-cyber <afanasevvlad829@gmail.com>
2026-02-07 11:05:35 +0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,20 @@
+window.APP_CONFIG = {
+  BOT_USERNAME: "schoolchat1_bot",
+
+  // CTA links
+  AIDACAMP_URL: "https://aidacamp.ru/",
+  CODIMS_URL: "https://codims.ru/",
+
+  // ✅ Stats endpoint (working)
+  STATS_URL: "https://script.google.com/macros/s/AKfycbyKYHYE-05LcVAG-_bQFRwQgh6CkCA51IlfbTut5rH8BzluATdhtxd7-7pzAgislzqQ/exec",
+
+  // UTM base
+  UTM: {
+    utm_source: "telegram",
+    utm_medium: "miniapp",
+    utm_campaign: "schoolchat_test"
+  },
+
+  // Deep link to open mini app
+  startapp: "schoolchat"
+};
